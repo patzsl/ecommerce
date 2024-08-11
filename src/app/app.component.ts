d330c2c2270913d20f4148367685ce65d417c824
Gabriel M. Teixeira <gm.teixeira@icloud.com>
2024-08-11 11:30:19 -0300

    chore(app): remove unused starter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { LayoutModule } from '@ecommerce/layout';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, LayoutModule],
+  imports: [RouterModule, LayoutModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
