68ab2b51242cf0c300a460236018d9a2199f9ecf
Gabriel <147352311+patzsl@users.noreply.github.com>
2024-08-21 15:36:06 -0300

    feat(layout): add href to title

--- modules/feature/layout/src/lib/header/header.component.spec.ts
+++ modules/feature/layout/src/lib/header/header.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '@ecommerce/layout';
 
 const mockTitle = 'Mock Title';
@@ -9,6 +10,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterModule.forRoot([])],
       declarations: [HeaderComponent],
     }).compileComponents();
 
@@ -29,5 +31,10 @@ describe('HeaderComponent', () => {
     component.title = 'Another title';
     fixture.detectChanges();
     expect(header.textContent).toContain('Another title');
+  });
+
+  it('should redirect to "/" when title is is clicked', () => {
+    const anchor: HTMLAnchorElement = fixture.nativeElement.querySelector('a');
+    expect(anchor.getAttribute('href')).toBe('/');
   });
 });
