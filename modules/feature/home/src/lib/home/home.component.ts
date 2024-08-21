330df45dc082c8be1c018e05798f8fdc9b9beb7c
Gabriel <147352311+patzsl@users.noreply.github.com>
2024-08-21 17:06:03 -0300

    recommended products

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { mockProducts } from '@ecommerce/data-access';
 
 @Component({
   selector: 'lib-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  products = mockProducts;
+  isLoading = true;
+
+  onImageLoad(event: Event) {
+    const target = event.target as HTMLImageElement;
+    this.isLoading = false; // Alterna para falso quando a imagem terminar de carregar
+  }
+}
